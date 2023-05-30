8aaa5a57bbec46a43ab24398f881367493874454
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2023-05-30 17:35:51 +0400

    Base package added

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# json-deep-sort
-A comprehensive utility for sorting JSON objects by keys, supporting both synchronous and asynchronous operations.
+# JSON Deep Sort
+
+![npm](https://img.shields.io/npm/v/json-deep-sort)
+![license](https://img.shields.io/npm/l/json-deep-sort)
+
+JSON Deep Sort is a comprehensive package that provides flexible sorting of JSON objects by keys. This
+TypeScript-written package can handle deeply nested objects and arrays, and offers both synchronous and asynchronous
+operations.
+
+## Features
+
+- Efficiently sorts keys in JSON objects and arrays.
+- Handles deeply nested JSON objects and arrays.
+- Provides both synchronous and asynchronous sorting operations.
+- Written in TypeScript for robust typing and error checking.
+
+## Installation
+
+Using npm:
+
+```shell
+npm install json-deep-sort
+```
+
+Using yarn:
+
+```shell
+yarn add json-deep-sort
+```
+
+## Usage
+
+Import JSON Deep Sort in your TypeScript file:
+
+```typescript
+import { sort } from '@tamtamchik/json-deep-sort';
+```
+
+Here is an example of sorting a JSON object:
+
+```typescript
+let data = {
+    b: 'b',
+    a: 'a',
+    c: {
+        d: 'd',
+        c: 'c',
+        a: 'a',
+        b: 'b'
+    }
+};
+let sortedData = sort(data, true);
+console.log(sortedData); // { a: 'a', b: 'b', c: { a: 'a', b: 'b', c: 'c', d: 'd' } }
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+JSON Deep Sort is [MIT licensed](./LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+/**
+ * Enumeration of data types.
+ * @enum {string}
+ */
+enum DataType {
+  OBJECT = 'OBJECT',
+  ARRAY = 'ARRAY',
+  STRING = 'STRING',
+  OTHER = 'OTHER',
+}
+
+/**
+ * Function to determine the data type of the provided data.
+ * @param {any} data - The data to be checked.
+ * @returns {DataType} The DataType value corresponding to the data's type.
+ */
+function getType (data: any): DataType {
+  const objectConstructor = {}.constructor
+  const arrayConstructor = [].constructor
+  const stringConstructor = ''.constructor
+
+  if (data && data.constructor === objectConstructor) {
+    return DataType.OBJECT
+  } else if (data && data.constructor === arrayConstructor) {
+    return DataType.ARRAY
+  } else if (data && data.constructor === stringConstructor) {
+    return DataType.STRING
+  } else {
+    return DataType.OTHER
+  }
+}
+
+/**
+ * Function to sort JSON data.
+ * @param {any} data - The data to be sorted.
+ * @param {boolean} [asc=true] - Whether to sort in ascending order.
+ * @returns {any} The sorted data.
+ * @throws {Error} If the data is not an object or array.
+ */
+export function sort (data: any, asc = true): any {
+  switch (getType(data)) {
+    case DataType.ARRAY:
+      return data.map((d: any) => getType(d) === DataType.OBJECT || getType(d) === DataType.ARRAY ? sort(d, asc) : d)
+
+    case DataType.OBJECT:
+      const keys = Object.keys(data).sort()
+      if (!asc) keys.reverse()
+      return keys.reduce((newData: any, key: string) => {
+        if (getType(data[key]) === DataType.OBJECT || getType(data[key]) === DataType.ARRAY)
+          newData[key] = sort(data[key], asc)
+        else
+          newData[key] = data[key]
+        return newData
+      }, {})
+
+    default:
+      throw new Error('Invalid data type: expected an object or array.')
+  }
+}
